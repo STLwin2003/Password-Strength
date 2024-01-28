e64634b88cccd4198777261f2ffc9e15ffcd8be5
STLwin2003 <shinthantlwin70@gmail.com>
2024-01-28 16:11:51 +0630

    all codes are done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let password = document.getElementById("password");
+let message = document.getElementById("message");
+let strength = document.getElementById("strength");
 
+password.addEventListener("input", () => {
+  if (password.value.length > 0) {
+    message.style.display = "block";
+  } else {
+    message.style.display = "none";
+  }
+
+  if (password.value.length <= 4) {
+    strength.innerHTML = " weak";
+    message.style.color = "red";
+  } else if (password.value.length <= 7) {
+    strength.innerHTML = " medium";
+    message.style.color = "orange";
+  } else {
+    strength.innerHTML = " strong";
+    message.style.color = "green";
+  }
+});
